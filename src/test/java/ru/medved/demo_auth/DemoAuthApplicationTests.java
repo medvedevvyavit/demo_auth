1c70948903f762e07ae6269ab3ee52d7eebb909e
вячеслав медведев <medvedev.vv@reliab.tech>
2023-01-24 10:16:08 +0300

    create application demo_auth

---
+++ src/test/java/ru/medved/demo_auth/DemoAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.medved.demo_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
